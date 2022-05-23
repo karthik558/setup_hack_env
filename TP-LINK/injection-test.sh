a5338f9f7d66b1f297f2b0e602fb0b5d1ee57a16
KARTHIK LAL <karthik.lal558@gmail.com>
2022-05-23 08:53:06 +0530

    script: debug: fix runtime error while injecting-packets on interface-NOS
    Signed-off-by: KARTHIK LAL <karthik.lal558@gmail.com>

--- TP-LINK/injection-test.sh
+++ TP-LINK/injection-test.sh
@@ -24,19 +24,19 @@ echo "Select the correct interface to test packet-injection ! (0/1)"
 read -r answer
 if [[ $answer == 0 ]]; then
    echo "Injecting packets on interface 0"
-   ifconfig wlan0 down
-   iwconfig wlan0 mode monitor
-   ifconfig wlan0 up
-   iwconfig wlan0
+   ifconfig wlan0 down;
+   iwconfig wlan0 mode monitor;
+   ifconfig wlan0 up;
+   iwconfig wlan0;
    echo "Injecting packets on interface 0"
    aireplay-ng --test wlan0
    echo "Packet injection test done on interface 0 and its working fine"
 else 
    echo "Injecting packets on interface 1"
-   ifconfig wlan1 down
-   iwconfig wlan1 mode monitor
-   ifconfig wlan1 up
-   iwconfig wlan1
+   ifconfig wlan1 down;
+   iwconfig wlan1 mode monitor;
+   ifconfig wlan1 up;
+   iwconfig wlan1;
    echo "Injecting packets on interface 1"
    aireplay-ng --test wlan1
    echo "Packet injection test done on interface 1 and its working fine"
